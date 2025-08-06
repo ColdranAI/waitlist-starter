86116984f1fa35904889d7ff766face1c99ddbc9
Arjun Aditya <meow@nermalcat69.dev>
2025-08-06 10:04:05 +0530

    iterate

--- drizzle/0001_bored_riptide.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "waitlist_entries" ADD COLUMN "ip_address" varchar(45) NOT NULL;--> statement-breakpoint
-ALTER TABLE "waitlist_entries" DROP COLUMN "user_agent";--> statement-breakpoint
-ALTER TABLE "waitlist_entries" DROP COLUMN "referrer";
